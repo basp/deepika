5501817f5c189d23ce0cf0acf773dcbe378d4335
Bas Pennings <baspster@gmail.com>
2019-10-11 18:43:38 +0100

    Added some basic tests for the arg parser

--- parser-test.rkt
+++ parser-test.rkt
@@ -3,4 +3,10 @@
 (require rackunit
          "parser.rkt")
 
+(check-equal?
+ (list "foo" "bar")
+ (parse/args "foo bar"))
 
+(check-equal?
+ (list "foo" "bar" "quux frotz" "zoz")
+ (parse/args "   foo    bar  \"quux frotz\"    zoz  "))
