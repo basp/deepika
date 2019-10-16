ab2a3ddf1f57915c149a62d896838f294774a860
Bas Pennings <baspster@gmail.com>
2019-10-16 21:01:59 +0100

    VM basics

--- vm.rkt
+++ vm.rkt
@@ -1,6 +1,11 @@
 #lang racket
 
+(struct state (env))
+
 (define stack null)
+
+(define (notify x)
+  (displayln (string-append "frotz: " x)))
 
 (define (push! x)
   (set! stack (cons x stack)))
@@ -10,12 +15,24 @@
   (set! stack (cdr stack))
   x)
 
-(define (pop/set-var! name env)
-  (hash-set! env name (pop!)))
-
 (define (push/get-var! name env)
   (push! (hash-ref env name)))
 
-(define vm%
-  (class object%
-    (super-new)))
+(define (pop/set-var! name env)
+  (hash-set! env name (pop!)))
+
+(define (get-prop!)
+  (let ([prop (pop!)]
+        [obj (pop!)])
+    (displayln 'ok)))
+
+(define (set-prop!)
+  (let ([val (pop!)]
+        [prop (pop!)]
+        [obj (pop!)])
+    (displayln 'ok)))
+
+(define (call-bi!)
+  (let ([args (pop!)]
+        [fn (pop!)])
+    (apply fn args)))
