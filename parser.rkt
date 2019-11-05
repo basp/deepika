cb5c16e17e20ed34395bd93888e912afd42d8c67
Bas Pennings <baspster@gmail.com>
2019-11-05 21:12:52 +0100

    Removed brag dependencies forever

--- parser.rkt
+++ parser.rkt
@@ -1,9 +1,9 @@
 #lang racket/base
 
 (require srfi/13
-         br-parser-tools/lex
-         br-parser-tools/yacc
-         (prefix-in : br-parser-tools/lex-sre))
+         parser-tools/lex
+         parser-tools/yacc
+         (prefix-in : parser-tools/lex-sre))
 
 (define-tokens value-tokens
   (INTEGER
@@ -20,6 +20,7 @@
 
 (define-empty-tokens symbol-tokens
   (COMMA
+   COLON
    LBRACE
    RBRACE
    LPAREN
@@ -51,6 +52,7 @@
    [(eof) 'EOF]
    [#\. (token-DOT)]
    [whitespace (return-without-pos (moo-lex input-port))]
+   [":" (token-COLON)]
    ["," (token-COMMA)]
    ["{" (token-LBRACE)]
    ["}" (token-RBRACE)]
@@ -64,7 +66,7 @@
    [">=" (token-GE)]
    ["in" (token-IN)]
    ["=>" (token-ARROW)]
-   [(char-set "?<>+-*/%^!:$")
+   [(char-set "?<>+-*/%^!:$=")
     (string->symbol lexeme)]
    [name
     (token-ID lexeme)]
@@ -83,7 +85,9 @@
 (struct add (lhs rhs) #:transparent)
 (struct sub (lhs rhs) #:transparent)
 (struct mul (lhs rhs) #:transparent)
+(struct div (lhs rhs) #:transparent)
 (struct neg (exp) #:transparent)
+(struct set (lhs rhs) #:transparent)
 (struct objid (val) #:transparent)
 (struct arglist (val) #:transparent)
 (struct condexpr (pred then else) #:transparent)
@@ -97,13 +101,15 @@
    (tokens value-tokens keyword-tokens op-tokens symbol-tokens)
    (error
     (λ (tok-ok? tok-name tok-value start-pos end-pos)
-      (displayln start-pos)))
+      (displayln start-pos)
+      (displayln tok-ok?)
+      (displayln tok-name)))
    (precs (right =)
           (left - +)
           (left * / %)
           (right ^)
           (left ! NEG)
-          (nonassoc ? PIPE DOT))
+          (nonassoc ? PIPE DOT COLON))
    (grammar
     (start [() #f]
            [(expr) $1])
@@ -112,16 +118,35 @@
     (ne-arglist [(expr) (list $1)]
                 [(ne-arglist COMMA expr)
                  (cons $3 $1)])
-    (expr [(INTEGER) (const $1)]
-          [(FLOAT) (const $1)]
-          [(STRING) (const $1)]
-          [(OBJECT) (const (objid $1))]
-          [(ID) (id $1)]
-          [(expr + expr) (add $1 $3)]
-          [(expr - expr) (sub $1 $3)]
-          [(expr * expr) (mul $1 $3)]
-          [(- expr) (prec NEG) (neg $2)]
-          [(LPAREN expr RPAREN) $2]
+    (expr [(INTEGER)
+           (const $1)]
+          [(FLOAT)
+           (const $1)]
+          [(STRING)
+           (const $1)]
+          [(OBJECT)
+           (const (objid $1))]
+          [(ID)
+           (id $1)]
+          [($ ID)
+           (propref (objid 0) $2)]
+          [(expr COLON ID LPAREN arglist RPAREN)
+           (list $1 (id $3) $5)]
+          [(expr + expr)
+           (add $1 $3)]
+          [(expr - expr)
+           (sub $1 $3)]
+          [(expr * expr)
+           (mul $1 $3)]
+          [(expr / expr)
+           (div $1 $3)]
+          [(expr = expr)
+           (set $1 $3)]
+          [(- expr)
+           (prec NEG)
+           (neg $2)]
+          [(LPAREN expr RPAREN)
+           $2]
           [(LBRACE arglist RBRACE)
            (arglist $2)]
           [(expr ? expr PIPE expr)
@@ -130,3 +155,7 @@
            (propref $1 $4)]
           [(expr DOT ID)
            (propref $1 (id $3))]))))
+
+(define (parse/string s)
+  (define ip (open-input-string s))
+  (moo-parse (λ () (moo-lex ip))))
